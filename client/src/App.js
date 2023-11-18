7c3abac9880517b6518a3bf79c9b94533e7e21c6
A-karkaih <akarkaih@gmail.com>
2023-11-18 21:37:52 +0100

    frontend architecture and react redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
-
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
+import HomePage from './scenes/homePage';
+import ProfilePage from './scenes/profilePage';
+import LoginPage from './scenes/loginpage';
+import NavBar from './scenes/navbar';
 function App() {
   return (
     <div className="app">
-    App
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='/home' element={<HomePage />} />
+        <Route path='/profile/:userId' element={<ProfilePage />} />
+    </Routes>
+    
+      </BrowserRouter>
+
     </div>
   );
 }
